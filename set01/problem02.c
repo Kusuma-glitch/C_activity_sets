7d28a26ee8b3216b419ee630de595f59a9563b42
Kusuma-glitch <141307769+Kusuma-glitch@users.noreply.github.com>
2024-01-23 09:56:55 +0530

    test

--- set01/problem02.c
+++ set01/problem02.c
@@ -2,7 +2,12 @@
 int main()
 {
     int a,b,sum;
-    printf("Enter two numbers");
+    printf("Enter the value of number1:");
+    scanf("%d",&a);
+    printf("Enter the value of number2:");
+    scanf("%d",&b);
     sum=a+b;
+    printf("The sum of %d and %d is %d", a, b,sum);
     return 0;
+    
 }
