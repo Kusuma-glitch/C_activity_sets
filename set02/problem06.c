f79d2a5eb065a3ca2a500461409fbfe1ca4c2acf
Kusuma-glitch <141307769+Kusuma-glitch@users.noreply.github.com>
2024-02-25 22:15:32 +0530

    test

---
+++ set02/problem06.c
@@ -0,0 +1,38 @@
+ //Write a program to reverse a string.
+
+#include<stdio.h>
+void input_string(char *a);
+void str_reverse(char *str, char *rev_str);
+void output(char *a, char *reverse_a);
+
+int main()
+{
+  char str[100], rev_str[100];
+  input_string(str);
+  str_reverse(str, rev_str);
+  output(str, rev_str);
+  return 0;
+}
+
+void input_string(char *a)
+{
+    printf("Enter a string:");
+    scanf("%s",a);
+}
+
+void str_reverse(char *str, char *rev_str)
+{
+    int length = strlrn(str);
+    int i, j;
+    for (i=0, j=length-1; i<length; i++, j--)
+    {
+        rev_str[i]=str[j];
+    }
+    rev_str[i]='\0';
+}
+
+void output(char *a, char *reverse_a)
+{
+    printf("Original String is : %s\n",a);
+    printf("Reversed Sting is: %s\n",reverse_a);
+}
