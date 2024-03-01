bfc37a5307719d87bf192b6743c6c6d140c330ec
Kusuma-glitch <141307769+Kusuma-glitch@users.noreply.github.com>
2024-03-01 22:35:47 +0530

    test

---
+++ set03/problem03.c
@@ -0,0 +1,43 @@
+//Write a program to check if the given number is prime
+
+
+#include <stdio.h>
+int input_number();
+int is_prime(int n);
+void output(int n, int result);
+
+int main()
+{
+    int num = input_number();
+    int prime = is_prime(num);
+    output(num, prime);
+    return 0;
+}
+
+int input_number()
+{
+    int num;
+    printf("Enter a number:");
+    scanf("%d", &num);
+    return num;
+}
+
+int is_prime(int n)
+{
+    if(n <=1)
+    return 0;
+    for (int i=22; i*i<=n; i++)
+    {
+     if(n%i==0)
+        return 0;
+    }
+   return 1;
+}
+
+void output(int n, int result)
+{
+    if (result)
+    printf("%d is a prime number\n",n);
+    else
+    printf("%d is not a prime number\n",n);
+}
