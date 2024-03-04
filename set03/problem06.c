ce4c8153d5f2cbb0d95d11357291007a4db8fc8a
Kusuma-glitch <141307769+Kusuma-glitch@users.noreply.github.com>
2024-03-04 23:40:15 +0530

    test

---
+++ set03/problem06.c
@@ -0,0 +1,52 @@
+//Write a program to find the index of a substring of a string
+
+#include <stdio.h>
+#include <string.h>
+
+// Function prototypes
+void input_string(char* a, char* b);
+int sub_str_index(char* string, char* substring);
+void output(char *string, char *substring, int index);
+
+int main() 
+{
+    char string[100], substring[100];
+    input_string(string, substring);
+    int index = sub_str_index(string, substring);
+    output(string, substring, index);
+    return 0;
+}
+
+void input_string(char* a, char* b) 
+{
+    printf("Enter the main string:");
+    scanf("%s", a);
+    printf("Enter the substring:");
+    scanf("%s", b);
+}
+
+int sub_str_index(char* string, char* substring) 
+{
+    int string_len = strlen(string);
+    int sub_len = strlen(substring);
+    for (int i = 0; i <= string_len - sub_len; i++) 
+    {
+        int j;
+        for (j = 0; j < sub_len; j++)
+        {
+            if (string[i + j] != substring[j])
+                break;
+        }
+        if (j == sub_len)
+            return i;
+    }
+    return -1;
+}
+
+void output(char *string, char *substring, int index) 
+{
+    if (index != -1)
+        printf("The index of '%s' in '%s' is %d\n", substring, string, index);
+    else
+        printf("'%s' not found in '%s'\n", substring, string);
+}
